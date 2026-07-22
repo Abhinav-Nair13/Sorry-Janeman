94dfa6ec6a1ec27999a048c09e1de973d93f4356
Abhinav-Nair13 <johncarlosjc37@gmail.com>
2026-07-22 18:24:16 +0530

    Implement name verification on button click
    Add name verification functionality with feedback messages.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const verifyBtn = document.getElementById("verifyBtn");
 
+verifyBtn.addEventListener("click", verifyName);
+
+function verifyName(){
+
+    const name = document
+    .getElementById("girlfriendName")
+    .value
+    .trim()
+    .toLowerCase();
+
+    const message = document.getElementById("message");
+
+    if(name === "Mansi Singh"){
+
+        message.style.color = "green";
+
+        message.innerHTML = "✅ Identity Verified ❤️";
+
+    }
+
+    else{
+
+        message.style.color = "red";
+
+        message.innerHTML = "❌ Unauthorized User";
+
+    }
+
+}
