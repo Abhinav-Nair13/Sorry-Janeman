f3b3bf635f4272ceb0f1a1f105049f7ec647d1bc
Abhinav-Nair13 <johncarlosjc37@gmail.com>
2026-07-22 20:21:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,5 @@
 const button = document.getElementById("verifyBtn");
 
 button.onclick = function () {
-
-    const input = document.getElementById("girlfriendName");
-    const message = document.getElementById("message");
-
-    if (input.value.toLowerCase().trim() === "bhoot") {
-
-        // Hide the message
-        message.innerHTML = "";
-
-        // Hide the verification form
-        document.querySelector(".card").style.display = "none";
-
-        // Show the search screen
-        document.getElementById("searchScreen").style.display = "block";
-
-        const loading = document.getElementById("loadingText");
-
-        loading.innerHTML = "🔍 Searching cutest girlfriend...";
-
-        setTimeout(function () {
-            loading.innerHTML = "💖 1 Match Found";
-        }, 1500);
-
-        setTimeout(function () {
-            loading.innerHTML = `
-                ❤️ 1 Match Found ❤️
-                <br><br>
-                Name : Bhoot
-                <br>
-                Status : Cutest Girlfriend Alive 👑
-                <br><br>
-                ✅ Identity Verified
-            `;
-        }, 3000);
-
-    }
-
-    else {
-
-        message.style.color = "red";
-
-        message.innerHTML = "❌ Unauthorized User";
-
-    }
-
-}
+    alert("Button clicked!");
+};
